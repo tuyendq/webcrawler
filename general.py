2486c620d27790af778b1cf8ec56b42288515acc
Tuyen Dang <tuyendq@gmail.com>
2019-05-16 22:46:03 +0700

    start crawl step

--- general.py
+++ general.py
@@ -25,8 +25,19 @@ def delete_file_contents(path):
     with open(path, 'w'):
         pass
 
+def file_to_set(file_name):
+	"""Read a file and convert each line to set items"""
+	results = set() # create an empty set
+	with open(file_name, 'rt') as f:
+		for line in f:
+			results.add(line.replace('\n',''))
+	return results
 
-
+def set_to_file(links, file_name):
+	"""Iterate through a set, each item will be a line in a file"""
+	with open(file_name, 'w') as f:
+		for l in sorted(links):
+			f.write(l+"\n")
 # Test 1
 create_project_dir('GeekUniversity')
 create_data_files('GeekUniversity', 'https://geek-university.com') 
